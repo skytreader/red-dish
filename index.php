a5564a7b2b6837ddd0f9ed2b6380d313e077b420
skytreader <chadestioco@gmail.com>
2012-10-17 00:26:49 +0800

    Preliminary layout design.

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>RedDish</title>
+	</head>
+	<link rel="stylesheet" href="red-dish.css" />
+	<script type="text/javascript" language="javascript" src="jquery.js">
+	</script>
+	<body>
+		<div class="body">
+		<h1 class="title">RED|DI<span style="color: #bc084b;">SH</span><h1>
+		<div class="app_block">
+			<h1 class="app_block_title">Redis Settings: </h1>
+			<div class="redis_settings" style="display: none;">
+				<strong>Host name:</strong><br />
+				<input type="text" name="hostname" /><br />
+				<strong>Port:</string><br />
+				<input type="text" name="port" />
+			</div>
+		</div>
+		</div>
+	</body>
+</html>
