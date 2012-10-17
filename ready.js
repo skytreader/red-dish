d5b0446c9ff4c56185277ffe5c9b085fc8d7e520
skytreader <chadestioco@gmail.com>
2012-10-17 23:58:07 +0800

    Tab switching still buggy but already sort of works.

--- ready.js
+++ ready.js
@@ -1,4 +1,6 @@
 var isSettingsDisplayed = false;
+// TODO Abstract with jQuery
+var tabSections = ["manual", "scripted"];
 
 function toggleSettings(){
 	if(!isSettingsDisplayed){
@@ -27,7 +29,28 @@ function setRedis(){
 	toggleSettings();
 }
 
+/**
+Enforced by convention: Tab headers are id'ed as section_head
+while bodies are id'ed as section_body.
+*/
+function toggleTab(section){
+	var limit = tabSections.length;
+	
+	for(var i = 0; i < limit; i++){
+		if(tabSections[i] != section){
+			$("#" + tabSections[i] + "_body").css({"display":"none"});
+			$("#" + tabSections[i] + "_head").removeClass("selected");
+		}
+	}
+	
+	$("#" + section + "_body").css({"display":"inline"});
+	$("#" + section + "_head").removeClass("unselected");
+	$("#" + section + "_head").addClass("selected");
+}
+
 $(document).ready(function(){
 	$("#redis_settings").click(toggleSettings);
 	$("[name=connect]").click(setRedis);
+	$("#manual_head").click(function(){toggleTab("manual");});
+	$("#scripted_head").click(function(){toggleTab("scripted")});
 })
