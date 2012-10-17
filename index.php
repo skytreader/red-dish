e490bba9d76b7d2803f676caada9d8da1cc94643
root <root@chad-MS-7529.(none)>
2012-10-17 16:24:28 +0800

    Added mock-ups for the tabbed options.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	</script>
 	<body>
 		<div class="body">
-			<h1 class="title">RED|DI<span style="color: #bc084b;">SH</span></h1>
+			<h1 class="title">RED|DI<span style="color: #000;">SH</span></h1>
 			<div class="app_block">
 				<span class="app_block_title">Redis Settings: </span>
 				<span class="block_button" id="redis_settings">+</span><br />
@@ -18,9 +18,26 @@
 					<br />
 					<strong>Host name:</strong><br />
 					<input type="text" name="hostname" /><br />
-					<strong>Port:</string><br />
+					<strong>Port:</strong><br />
 					<input type="text" name="port" /><br />
 					<input type="button" value="Connect" name="connect" />
+				</div>
+			</div>
+			<hr />
+			<div class="tab_headers">
+				<div class="tab selected">
+					<span class="app_block_title" id="manual_head">Manual Entry</span>
+				</div>
+				<div class="tab unselected">
+					<span class="app_block_title" id="scripted_head">Scripted Entry</span>
+				</div>
+			</div>
+			<div class="app_block" id="tabs">
+				<div class="tab_body selected_body" id="manual_body">
+					<p>This is the manual entry option.</p>
+				</div>
+				<div class="tab_body unselected_body" id="scripted_body">
+					<p>This is the scripted entry option.</p>
 				</div>
 			</div>
 		</div>
