9542d16efb3df2261801199d414e985bbcbf371f
root <root@chad-MS-7529.(none)>
2012-10-17 13:26:24 +0800

    Finished general behavior of Redis settings block.

--- index.php
+++ index.php
@@ -9,17 +9,20 @@
 	</script>
 	<body>
 		<div class="body">
-		<h1 class="title">RED|DI<span style="color: #bc084b;">SH</span></h1>
-		<div class="app_block" id="redis_settings">
-			<h1 class="app_block_title">Redis Settings: </h1>
-			<div class="redis_settings" style="display: none;">
-				<strong>Host name:</strong><br />
-				<input type="text" name="hostname" /><br />
-				<strong>Port:</string><br />
-				<input type="text" name="port" /><br />
-				<input type="button" value="Connect" />
+			<h1 class="title">RED|DI<span style="color: #bc084b;">SH</span></h1>
+			<div class="app_block">
+				<span class="app_block_title">Redis Settings: </span>
+				<span class="block_button" id="redis_settings">+</span><br />
+				<span id="settings_display"></span>
+				<div class="redis_settings" style="display: none;">
+					<br />
+					<strong>Host name:</strong><br />
+					<input type="text" name="hostname" /><br />
+					<strong>Port:</string><br />
+					<input type="text" name="port" /><br />
+					<input type="button" value="Connect" name="connect" />
+				</div>
 			</div>
-		</div>
 		</div>
 	</body>
 </html>
--- ready.js
+++ ready.js
@@ -3,13 +3,31 @@ var isSettingsDisplayed = false;
 function toggleSettings(){
 	if(!isSettingsDisplayed){
 		$(".redis_settings").css({"display":"inline"});
+		$("#redis_settings").html("-");
 	} else{
 		$(".redis_settings").css({"display":"none"});
+		$("#redis_settings").html("+");
 	}
 	
 	isSettingsDisplayed = !isSettingsDisplayed;
 }
 
+function displaySettings(){
+	var hostname = $("[name=hostname]")[0].value;
+	var port = $("[name=port]")[0].value;
+	$("#settings_display").html(hostname + ":" + port);
+}
+
+function connectRedis(){
+}
+
+function setRedis(){
+	displaySettings();
+	connectRedis();
+	toggleSettings();
+}
+
 $(document).ready(function(){
 	$("#redis_settings").click(toggleSettings);
-});
+	$("[name=connect]").click(setRedis);
+})
